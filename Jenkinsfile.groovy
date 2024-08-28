106ed6eea2d9a93dda77d94c7fd255821da34da3
Tri.Ta <tri.ta@ptnglobalcorp.com>
2024-08-28 11:03:18 +0700

    update jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,45 +1,53 @@
 pipeline {
-    agent {
-        docker {
-            image 'maven:latest'
-        }
+    agent any
+    
+    environment {
+        DOCKER_API_VERSION = "1.47"
+        APP_NAME = "hello-kenzan"
+        REGISTRY_HOST = "127.0.0.1:30400/"
     }
+    
     stages {
-        stage('Checkout SCM') {
+        stage('Checkout') {
             steps {
                 checkout scm
             }
         }
-        stage('Set Environment Variables') {
+        
+        stage('Set Build Variables') {
             steps {
                 script {
-                    env.DOCKER_API_VERSION = "1.23"
-                    
-                    sh "git rev-parse --short HEAD > commit-id"
-                    
-                    tag = readFile('commit-id').replace("\n", "").replace("\r", "")
-                    appName = "hello-kenzan"
-                    registryHost = "127.0.0.1:30400/"
-
-                    imageName = "${registryHost}${appName}:${tag}"
-                    env.BUILDIMG = imageName
+                    env.GIT_COMMIT_SHORT = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
+                    env.IMAGE_NAME = "${env.REGISTRY_HOST}${env.APP_NAME}:${env.GIT_COMMIT_SHORT}"
                 }
             }
         }
+        
         stage('Build') {
             steps {
-                sh "docker build -t ${env.BUILDIMG} -f applications/hello-kenzan/Dockerfile applications/hello-kenzan"
+                sh "docker build -t ${env.IMAGE_NAME} -f applications/${env.APP_NAME}/Dockerfile applications/${env.APP_NAME}"
             }
         }
+        
         stage('Push') {
             steps {
-                sh "docker push ${env.BUILDIMG}"
+                sh "docker push ${env.IMAGE_NAME}"
             }
         }
+        
         stage('Deploy') {
             steps {
-                kubernetesDeploy configs: "applications/${appName}/k8s/*.yaml", kubeconfigId: 'kenzan_kubeconfig'
+                kubernetesDeploy(
+                    configs: "applications/${env.APP_NAME}/k8s/*.yaml",
+                    kubeconfigId: 'kenzan_kubeconfig'
+                )
             }
+        }
+    }
+    
+    post {
+        always {
+            cleanWs()
         }
     }
 }
