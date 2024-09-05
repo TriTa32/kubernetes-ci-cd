a4547ba52a1e9541d2c107abb98484490d42601c
Tri.Ta <tri.ta@ptnglobalcorp.com>
2024-09-05 10:00:33 +0700

    update jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,7 +37,7 @@ pipeline {
         
         stage('Deploy') {
             steps {
-                sh "kubectl apply -f applications/${env.APP_NAME}/k8s/*.yaml"
+                sh "kubectl apply -f applications/${env.APP_NAME}/k8s/deployment.yaml"
                 // kubernetesDeploy(
                 //     configs: "applications/${env.APP_NAME}/k8s/*.yaml",
                 //     kubeconfigId: 'kenzan_kubeconfig'
